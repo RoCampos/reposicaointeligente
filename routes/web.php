5dc08ae11b395b6ee5a7fe1982d223b7ae7fb9a2
MireleSCos <mirele.si.co@outlook.com>
2018-07-11 18:00:54 -0300

    Rotas para : LOGIN, Padrão e Solicitar foram criadas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/pad', function () {
+    return view('padrao');
+})->name('pad');
+
+Route::get('/s_repo', function () {
+    return view('solicitar_repo');
+})->name('s_repo');
