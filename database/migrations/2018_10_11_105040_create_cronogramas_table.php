80505de7b9e57a0950d2fb1c9cdb14e98d293c26
MireleSCos <mirele.si.co@outlook.com>
2018-10-11 16:37:12 -0300

    Front-end adicionado com 90% de conclução e Migrações,models,factory e seeder implementados e adicionados

---
+++ database/migrations/2018_10_11_105040_create_cronogramas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCronogramasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cronogramas', function (Blueprint $table) {
+            $table->increments('id_cronograma');
+            $table->string('id_aluno');
+            $table->text('descricao');
+            $table->string('hora');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cronogramas');
+    }
+}
